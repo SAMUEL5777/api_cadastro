fe341cabecd5731dd2dfdfdd3eef7207e5f82a6f
SAMUEL5777 <teodoro.prado.samuel@escola.pr.gov.br>
2026-03-11 11:11:44 -0300

    auteracao

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express'); //servidor web
+const fs = require('fs'); //manipulação de arquivos
+const path = require('path'); //manipulação de caminhos
+
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+/*
+CLIENTES ENDPOINTS
+*/
+
+const clientesFile = path.join(__dirname, "clientes.json");
+
+function lerClientes() {
+    if(!fs.existsSync(clientesFile)){
+     return [];   
+    }
+
+    const dados = fs.readFileSync(clientesFile, 'utf8');
+
+ try{
+   return JSON.parse(dados) || [];
+ } catch (e) {
+   return [];
+ } 
+
+}
+
+function salvarClientes(clientes) {
+   fs.whiteFileSync(clientesFile,JSON.stringify(clientes,null,2), 'utf8');
+}
+
+
+app.post('/clientes',(req,res) =>{
+    const {cpf, nome, idade, endereco, bairro, contato } = req.body;
+    });
+    if(!cpf || !nome || !idade || !endereco || !bairro || !contato) {
+        return res.status(400).json({error: 'Todos os campos são obrigatórios'});
+    }
+    const clientes = lerClientes();
+    if(clientes.some(c=> c.cpf===cpf)){
+        return res.status(400).json({error: 'CPF já cadastrado'});
+    }
+
+    const novoCliente = {cpf, nome, idade, endereco, bairro, contato };
+    clientes.push(novoCliente);
+    salvarClientes(clientes);
+
+    res.status(201).json({message: "Cliente cadastrado com sucesso" });
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
