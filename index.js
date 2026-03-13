cba5f1a64c6c4af16b84deb07149711edc3a9343
SAMUEL5777 <teodoro.prado.samuel@escola.pr.gov.br>
2026-03-13 10:43:38 -0300

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require('express'); //servidor web
-const fs = require('fs'); //manipulação de arquivos
-const path = require('path'); //manipulação de caminhos
+const express = require('express');//servidor web
+const fs = require('fs');//manipulacao de arquivos
+const path = require('path');//manipulacao de caminhos
+
 
 const app = express();
 const port = 3000;
-app.use(express.json());
 
+app.use(express.json());
 /*
 CLIENTES ENDPOINTS
 */
@@ -13,45 +14,43 @@ CLIENTES ENDPOINTS
 const clientesFile = path.join(__dirname, "clientes.json");
 
 function lerClientes() {
-    if(!fs.existsSync(clientesFile)){
-     return [];   
-    }
-
-    const dados = fs.readFileSync(clientesFile, 'utf8');
-
- try{
-   return JSON.parse(dados) || [];
- } catch (e) {
-   return [];
- } 
-
+    
+if(!fs.existsSync(clientesFile)){
+    return [];
 }
-
+const dados = fs.readFileSync(clientesFile, 'utf8');
+try{
+    return JSON.parse(dados) || [];
+} catch (e) {
+    return [];
+}
+}
 function salvarClientes(clientes) {
-   fs.whiteFileSync(clientesFile,JSON.stringify(clientes,null,2), 'utf8');
+    fs.writeFileSync(clientesFile, JSON.stringify(clientes, null, 2), 'utf8');
+    
 }
-
-
-app.post('/clientes',(req,res) =>{
-    const {cpf, nome, idade, endereco, bairro, contato } = req.body;
-    });
-    if(!cpf || !nome || !idade || !endereco || !bairro || !contato) {
-        return res.status(400).json({error: 'Todos os campos são obrigatórios'});
-    }
-    const clientes = lerClientes();
-    if(clientes.some(c=> c.cpf===cpf)){
-        return res.status(400).json({error: 'CPF já cadastrado'});
-    }
-
-    const novoCliente = {cpf, nome, idade, endereco, bairro, contato };
-    clientes.push(novoCliente);
-    salvarClientes(clientes);
-
-    res.status(201).json({message: "Cliente cadastrado com sucesso" });
-
-
-
+app.post('/clientes', (req, res) => {
+   const {nome, email, cpf, data_nascimento,bairro, telefone} = req.body;
+   if (!nome || !email || !cpf || !data_nascimento || !bairro || !telefone) {
+    return res.status(400).json({ error: 'Caira uma bomba na tua casa' });
+   }
+   const clientes = lerClientes();
+   if (clientes.some(c => c.cpf === cpf)) {
+    return res.status(400).json({ error: 'CPF já cadastrado' });
+   }
+   const novoCliente = {
+    nome,
+    email,
+    cpf,
+    data_nascimento,
+    bairro,
+    telefone
+   };
+   clientes.push(novoCliente);
+   salvarClientes(clientes);
+   res.status(201).json({message: 'Cliente cadastrado com sucesso', cliente: novoCliente});
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
